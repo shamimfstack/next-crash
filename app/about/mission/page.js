5c984da69e6f9fc9dd70b9634f94effbb15408a8
shamimfstack <shamimfstack@gmail.com>
2024-04-06 10:36:39 +0600

    route and nested route created

---
+++ app/about/mission/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MissionPage = () => {
+    return (
+        <main className='mt-10'>
+            <div>
+                Mission page
+            </div>
+        </main>
+    );
+};
+
+export default MissionPage;
---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <main className='mt-10'>
+            <div>
+                About page
+            </div>
+        </main>
+    );
+};
+
+export default AboutPage;
